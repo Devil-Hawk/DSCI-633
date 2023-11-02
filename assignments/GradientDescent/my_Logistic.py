6b4fa24816fb857a2279a2c57c2bcbdf333f60fa
Devil-Hawk <ankitpunjabi89@gmail.com>
2023-11-02 17:24:09 -0400

    Assignment 9 Completed!

--- assignments/GradientDescent/my_Logistic.py
+++ assignments/GradientDescent/my_Logistic.py
@@ -3,44 +3,64 @@ import numpy as np
 
 class my_Logistic:
 
-    def __init__(self, learning_rate = 0.1, batch_size = 10, max_iter = 100, shuffle = False):
-        # Logistic regression: f(x) = 1 / (1+exp(-(w0+w*x))})
-        # Loss function is sum (f(x)-y)**2
-        # learning_rate: Learning rate for each weight update.
-        # batch_size: Number of training data points in each batch.
-        # max_iter: The maximum number of passes over the training data (aka epochs). Note that this is not max batches.
-        # shuffle: Whether to shuffle the data in each epoch.
+    def __init__(self, learning_rate=0.1, batch_size=10, max_iter=100, shuffle=False):
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.max_iter = max_iter
         self.shuffle = shuffle
 
     def fit(self, X, y):
-        # X: pd.DataFrame, independent variables
-        # y: list, np.array or pd.Series, dependent variables
+        #fitting the logistic regression model to training data
         data = X.to_numpy()
         d = data.shape[1]
-        # Initialize weights as all zeros
-        self.w = np.array([0.0]*d)
+        self.w = np.array([0.0] * d)
         self.w0 = 0.0
-        # write your code below
+        n = len(y)
+        for epoch in range(self.max_iter):
+            if self.shuffle:
+                data, y = self.shuffle_data(data, y)
+            batches = self.generate_batches(n)
+            for batch in batches:
+                X_train = data[batch]
+                y_train = y[batch]
+                self.w, self.w0 = self.sgd(X_train, y_train, self.w, self.w0)
+
+    def generate_batches(self, n):
+        #Generating batches of data for stocastic gradient descent
+        if self.batch_size >= n:
+            return [list(range(n))]
+        num_batches = n // self.batch_size
+        remainder = n % self.batch_size
+        batches = [list(range(i * self.batch_size, (i + 1) * self.batch_size)) for i in range(num_batches)]
+        if remainder > 0:
+            batches.append(list(range(num_batches * self.batch_size, n)))
+        return batches
+
+    def sgd(self, X, y, w, w0):
+        #performing sgd for a batch
+        wx = np.dot(X, w) + w0
+        fx = 1.0 / (1 + np.exp(-wx))
+        gradient_w = np.dot(X.T, fx - y) / X.shape[0]
+        gradient_w0 = np.sum(fx - y) / X.shape[0]
+        w -= self.learning_rate * gradient_w
+        w0 -= self.learning_rate * gradient_w0
+        return w, w0
+
+    def shuffle_data(self, X, y):
+        #shuffling data
+        indices = np.arange(len(y))
+        np.random.shuffle(indices)
+        return X[indices], y[indices]
 
     def predict_proba(self, X):
-        # X: pd.DataFrame, independent variables
-        # prob is a dict of prediction probabilities belonging to each categories
-        # return probs = f(x) = 1 / (1+exp(-(w0+w*x))}); a list of float values in [0.0, 1.0]
-        # write your code below
+        #classs probabilities for input
+        data = X.to_numpy()
+        wx = np.dot(data, self.w) + self.w0
+        fx = 1.0 / (1 + np.exp(-wx))
         return fx
 
     def predict(self, X):
-        # X: pd.DataFrame, independent variables, str
-        # return predictions: list of int values in {0, 1}
-        # write your code below
+        #Predict binary class labels
         probs = self.predict_proba(X)
-        predictions = [1 if prob >=0.5 else 0 for prob in probs]
+        predictions = [1 if prob >= 0.5 else 0 for prob in probs]
         return predictions
-
-
-
-
-
